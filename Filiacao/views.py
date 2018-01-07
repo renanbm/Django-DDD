e61d9862fd1b038a57ed1bc9b239f4ea474de030
Renan Ben Moshe <renan.moshe@gmail.com>
2018-01-07 20:44:01 -0200

    Criando a View de Detalhes

--- Filiacao/views.py
+++ Filiacao/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Homepage do App de Filiação.</h1>")
+
+
+def obterCliente(request, codCliente):
+    return HttpResponse("<h2>Detalhes para o cliente id: " + str(codCliente) + "</h2>")
