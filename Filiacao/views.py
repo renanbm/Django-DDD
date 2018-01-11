ed994e5acd57d8168bc48b8b857f74fb0c3a8435
Renan Ben Moshe <renan.moshe@gmail.com>
2018-01-10 22:57:44 -0200

    Incluindo templates

--- Filiacao/views.py
+++ Filiacao/views.py
@@ -1,17 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Cliente, Endereco
 
 
 def index(request):
     clientes = Cliente.objects.all()
-
-    html = ''
-
-    for cliente in clientes:
-        url = '/filiacao/' + str(cliente.CodCliente) + '/'
-        html += '<a href="' + url + '">' + cliente.Nome + '</a><br>'
-
-    return HttpResponse(html)
+    template = loader.get_template('Filiacao/index.html')
+    context = {
+        'clientes': clientes,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def obterCliente(request, codCliente):
